c5609dd29b405a7c7d0be8a5578ce7c7ce53aaf0
Nina <nina.roehm@googlemail.com>
2018-11-06 14:06:50 +0100

    Button init & CleanUp

--- Source/IC-Test/IC-Test/IC-Test.ino
+++ Source/IC-Test/IC-Test/IC-Test.ino
@@ -3,43 +3,52 @@
 #include "RepeatButton.h"
 #include "ClickButton.h"
 
+#
 /*
  Name:		IC_Test.ino
  Created:	24.10.2018 12:43:21
  Author:	mohr_s
 */
 
-#define button1 45
-#define button2 6
+#pragma region buttonSetup 
 
+#define ButtonCount 4
+#define Up 0
+#define UpPort 2
+#define Right 1
+#define RightPort 3
+#define Down 2
+#define DownPort 4
+#define Left 3
+#define LeftPort 5
 
-AbstractDebouncedButton* buttons[4];
+AbstractDebouncedButton* buttons[ButtonCount];
+
+void setupButtons() {
+	buttons[Up] = new RepeatButton(UpPort);
+	buttons[Right] = new ClickButton(RightPort);
+	buttons[Down] = new RepeatButton(DownPort);
+	buttons[Left] = new ClickButton(LeftPort);
+}
+void handleButtons() {
+	for (int i = 0; i < ButtonCount; i++) {
+		buttons[i]->handleButton();
+	}
+}
+#pragma endregion
+
 int zaehler = 0;
 int lasthit = 0;
 // the setup function runs once when you press reset or power the board
 void setup() {
 
 	Serial.begin(9600);
-	buttons[0] = new ClickButton(10);
-	buttons[1] = new RepeatButton(11);
-	
+	setupButtons();
 }
 int count1 = 0;
 int count2 = 0;
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	buttons[0]->handleButton();
-	buttons[1]->handleButton();
-
-	if (buttons[0]->isPressed()) {
-		Serial.println("Boo1!");
-		Serial.println(++count1);
-
-	}
-	if (buttons[1]->isPressed()) {
-		Serial.println("Boo2!");
-		Serial.println(++count2);
-
-	}
+	handleButtons();
 }
